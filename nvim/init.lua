506f06ae66039004dc986b581e2cae063754f244
Syed Ali Raza <isyedaliraza@gmail.com>
2024-12-26 12:37:57 +0000

    Update nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,20 @@
+print("Neovim is awesome!")
 require("config.lazy")
 
 -- Set indentation to 4 spaces
 vim.opt.shiftwidth = 4
 
-if true then
-    print("Wow!")
-end
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
